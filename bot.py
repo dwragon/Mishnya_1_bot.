ba1b18bdf8c98578123c3b6b8a51a05a5b9c1a56
dwragon <36308552+dwragon@users.noreply.github.com>
2020-12-25 23:04:13 +0300

    puuush

--- bot.py
+++ bot.py
@@ -1,48 +1,78 @@
-import json
-import requests
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+
+
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-PORT = int(os.environ.get('PORT', 5000))
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-level=logging.INFO)
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
-TOKEN = '1474388780:AAG4NEQIO-5BII6OHpSLKQqJEkyPzZ1n8bo'
+
+PORT = int(os.environ.get('PORT', '8443'))
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+
 
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
+
+
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
+
 def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
-   # Get the dispatcher to register handlers
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(
+        '1474388780:AAG4NEQIO-5BII6OHpSLKQqJEkyPzZ1n8bo', use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
+
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
-   # log all errors
+
+    # log all errors
     dp.add_error_handler(error)
+
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
-    updater.bot.setWebhook('https://appforlab.herokuapp.com/' + TOKEN)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path='1474388780:AAG4NEQIO-5BII6OHpSLKQqJEkyPzZ1n8bo')
+    # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
+    updater.bot.set_webhook('https://appforlab.herokuapp.com/' + '1474388780:AAG4NEQIO-5BII6OHpSLKQqJEkyPzZ1n8bo')
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
